14ab37fb193c2d8c1c931d1337716fa414fd24ba
Tohru7w7 <111470953+Tohru7w7@users.noreply.github.com>
2025-11-05 14:21:34 -0600

    holamundo pl

---
+++ logica/holamundo.pl
@@ -0,0 +1,14 @@
+valioso(oro).
+valioso(plata).
+valioso(diamante).
+le_gusta(juan,maria).
+tiene(juan,libro).
+tiene(maria,libro).
+ 
+%Diagrama de Progenitores
+progenitor(clara,jose).
+progenitor(tomas,jose).
+progenitor(tomas,isabel).
+progenitor(jose,ana).
+progenitor(jose,patricia).
+progenitor(patricia,jaime).
